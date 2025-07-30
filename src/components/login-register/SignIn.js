002ffb7f5c7d90115ab5ef8c4f7321049f621be9
Harsh Thorat <harshthorat18@gmail.com>
2025-07-30 12:50:38 +0530

    Update SignIn.js

--- src/components/login-register/SignIn.js
+++ src/components/login-register/SignIn.js
@@ -41,6 +41,7 @@ const SignIn = () => {
     } catch (err) {
       console.error("Login error:", err.response?.data);
        const msg = err.response?.data?.message;
+      console.error("Full login error message:", msg);
   if (Array.isArray(msg)) {
     setError(msg); // array of error objects
   } else {
